3bc8bd29dd20458d8f7c44430db45600801bfd7a
Jaapio <jaap@ijaap.nl>
2017-05-01 13:59:21 +0200

    Initial setup

---
+++ src/Skeleton.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Polder Knowledge / Authentication (https://polderknowledge.com)
+ *
+ * @link https://github.com/polderknowledge/authentication for the canonical source repository
+ * @copyright Copyright (c) 2017 Polder Knowledge (https://polderknowledge.com)
+ * @license https://github.com/polderknowledge/authentication/blob/master/LICENSE.md MIT
+ */
+
+namespace PolderKnowledge\Authentication;
+
+class Skeleton
+{
+}
---
+++ tests/SkeletonTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Polder Knowledge / Authentication (https://polderknowledge.com)
+ *
+ * @link https://github.com/polderknowledge/authentication for the canonical source repository
+ * @copyright Copyright (c) 2017 Polder Knowledge (https://polderknowledge.com)
+ * @license https://github.com/polderknowledge/authentication/blob/master/LICENSE.md MIT
+ */
+
+namespace PolderKnowledge\Authentication;
+
+use PHPUnit_Framework_TestCase;
+
+class SkeletonTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
